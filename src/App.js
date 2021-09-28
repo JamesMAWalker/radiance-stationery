6785896cce55ced20f0470fd3ca94df0dbac3447
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2021-09-29 00:58:27 +0700

    Remove commented code and add packageItems state to App.js.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import {
   Switch,
   BrowserRouter as Router,
 } from 'react-router-dom'
-import axios from 'axios'
 
 import { DocumentEditor } from './components/editor'
 import { Home } from './components/home'
@@ -14,20 +13,12 @@ import { InvoiceTable } from './components/invoice'
 import { Receipt } from './components/receipt'
 import { Contract } from './components/contract';
 
-// const getUsersData = () => {
-//   return axios
-//     .get('https://randomuser.me/api/?results=5')
-//     .then((res) => {
-//       return res.data.results
-//     })
-//     .catch((err) => 
-// }
-
 function App() {
   const [pageCount, setPageCount] = useState(0)
   const [rowCount, setRowCount] = useState(1)
   const [rowCollapsed, setRowCollapsed] = useState(false)
   const [isDeposit, setIsDeposit] = useState(false)
+  const [packageItems, setPackageItems] = useState([])
   const templateRoutes = [
     {
       path: '/letterhead',
@@ -73,61 +64,12 @@ function App() {
       Component: Contract,
       props: {
         docType: 'Contract',
+        packageItems,
+        setPackageItems,
       },
     },
   ]
 
-  // const [userData, setUserData] = useState([])
-  // const [flattenedLocations, setFlattenedLocations] = useState([])
-  // 
-
-  // const extractObjectKeys = (object) => {
-  //   let objectKeys = []
-
-  //   Object.keys(object).forEach(objectKey => {
-  //     const value = object[objectKey]
-  //     if (typeof value !== 'object') {
-  //       objectKeys.push(objectKey)
-  //     } else {
-  //       objectKeys = [...objectKeys, ...extractObjectKeys(value)]
-  //     }
-  //   })
-  //   return objectKeys
-  // }
-
-  // const flattenLocations = (locations) => {
-  //   const flattenedLocationHeaders = extractObjectKeys(locations[0])
-  //   const data = []
-  //   for (const { street, coordinates, timezone, ...rest } of locations) {
-  //     data.push({
-  //       ...rest,
-  //       name: street.name,
-  //       number: street.number,
-  //       latitude: coordinates.latitude,
-  //       longitude: coordinates.longitude,
-  //       offset: timezone.offset,
-  //       description: timezone.description,
-  //     })
-  //   }
-    
-  //   return { headers: flattenedLocationHeaders, data}
-  // }
-
-  // useEffect(() => {
-  //   getUsersData()
-  //     .then((userAPIData) => {
-  //       setUserData(userAPIData)
-  //       setFlattenedLocations(
-  //         flattenLocations(
-  //           userAPIData.map(({ location }) => location)
-  //         )
-  //       )
-  //     })
-  // }, [])
-
-  // useEffect(() => {
-  //   
-  // }, [flattenedLocations])
   const [isMobile, setIsMobile] = useState(false)
 
    useEffect(() => {
@@ -140,39 +82,6 @@ function App() {
       <Router>
         <Switch>
           <Route path='/' exact>
-            {/* <div
-              style={{
-                minHeight: '40vh',
-                width: '100%',
-                display: 'flex',
-                flexDirection: 'column',
-              }}
-            >
-              <h1>Here be the Data:</h1>
-              <table>
-                <thead>
-                  <tr>
-                    {flattenedLocations.headers &&
-                      flattenedLocations.headers.map(
-                        (locationString, idx) => (
-                          <th key={idx}>{locationString}</th>
-                        )
-                      )}
-                  </tr>
-                </thead>
-                <tbody>
-                  {flattenedLocations.data &&
-                    flattenedLocations.data.map((location, locationIdx) => (
-                      <tr key={locationIdx}>
-                        {flattenedLocations.headers.map((header, cellIdx) => {
-                          return <td key={cellIdx}>{location[header]}</td>
-                        })}
-                      </tr>
-                    ))
-                  }
-                </tbody>
-              </table>
-            </div> */}
             <Home />
           </Route>
           {templateRoutes.map(
